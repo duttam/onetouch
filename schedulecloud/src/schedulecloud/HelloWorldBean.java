c1a6fe4220a82333a3c9e353ae48ac50caf6be20
Manas Dutta <manas.dutta@dfyoung.com>
2014-08-03 15:13:52 -0400

    initial web project for schedule cloud

---
+++ schedulecloud/src/schedulecloud/HelloWorldBean.java
@@ -0,0 +1,32 @@
+package schedulecloud;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="helloWorld")
+@ViewScoped
+public class HelloWorldBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String string;
+	@PostConstruct
+	public void init(){
+		string = "hello world";
+	}
+
+	public String getString() {
+		return string;
+	}
+
+	public void setString(String string) {
+		this.string = string;
+	}
+	
+	
+}
