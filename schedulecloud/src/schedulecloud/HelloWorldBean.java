18315851e834412c41142e43b7448fe1113320d4
Manas Dutta <manas.dutta@dfyoung.com>
2014-08-07 16:28:54 -0400

    added new string for WPC

--- schedulecloud/src/schedulecloud/HelloWorldBean.java
+++ schedulecloud/src/schedulecloud/HelloWorldBean.java
@@ -15,9 +15,11 @@ public class HelloWorldBean implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	private String string;
+	private String stringFromWPCBranch;
 	@PostConstruct
 	public void init(){
 		string = "this is a sinple demo test";
+		stringFromWPCBranch = "We added this string from WPC Branch";
 	}
 
 	public String getString() {
@@ -26,6 +28,14 @@ public class HelloWorldBean implements Serializable{
 
 	public void setString(String string) {
 		this.string = string;
+	}
+
+	public String getStringFromWPCBranch() {
+		return stringFromWPCBranch;
+	}
+
+	public void setStringFromWPCBranch(String stringFromWPCBranch) {
+		this.stringFromWPCBranch = stringFromWPCBranch;
 	}
 	
 	
